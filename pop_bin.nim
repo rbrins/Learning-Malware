43ec3062c46f924fed50cf69f40bd69ff5d86728
Russell Brinson <rbrins@icloud.com>
2022-02-03 00:11:15 -0500

    initial commit

---
+++ pop_bin.nim
@@ -0,0 +1,15 @@
+#[
+    Author: Marcello Salvati, Twitter: @byt3bl33d3r
+    License: BSD 3-Clause
+]#
+
+type
+    HANDLE* = int
+    HWND* = HANDLE
+    UINT* = int32
+    LPCSTR* = cstring
+
+proc MessageBox*(hWnd: HWND, lpText: LPCSTR, lpCaption: LPCSTR, uType: UINT): int32 
+  {.discardable, stdcall, dynlib: "user32", importc: "MessageBoxA".}
+
+MessageBox(0, "Hello, world !", "Nim is Powerful", 0)
